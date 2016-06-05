a2787b3a2ad0f02f6f89943b55a9f24b07633e87
GabrielAlacchi <alacchi.g@gmail.com>
2016-06-05 00:25:13 -0400

    Added validation to response form using token

--- routes/response.js
+++ routes/response.js
@@ -4,30 +4,39 @@ var ticketDB = require('../source/TicketDatabase');
 
 /* GET response page. */
 router.get('/', function(req, res, next) {
-  var ticketId = req.param('id');
-  ticketDB.fetchTicketRecord(ticketId, function(ticketData) {
 
-      if (ticketData.statusCode == 1) {
-          res.render('closedTicket', {ticketId: ticketId, ticketTitle: title});
-          return;
-      }
+    var ticketId = req.param('id');
+    var token = req.param('token');
 
-      ticketDB.getTicketMessageHistory(ticketId, function(results) {
-              var history = "";
-              for (var i = 0; i < results.length; ++i) {
-                  var result = results[i];
-                  history += result["msg_sender"].replace("alfred", ticketData.name) + ": " + result["msg_text"] + "*np*";
-              }
+    ticketDB.validTicketTokenPair(ticketId, token, function(result) {
+       if (!result) {
+           //Failed validation
+           res.render('failedValidation');
+       } else {
+           ticketDB.fetchTicketRecord(ticketId, function(ticketData) {
+               if (ticketData.statusCode == 1) {
+                   res.render('closedTicket', {ticketId: ticketId, ticketTitle: title});
+                   return;
+               }
 
-              history = history.replace("\n", "*br*");
-              res.render('userResponse', { historyBlock: history, name: ticketData.name, title: ticketData.title });
-          },
-          function (err) {
-              res.render('error');
-          });
-  }, function(err) {
-      res.render('error');
-  });
+               ticketDB.getTicketMessageHistory(ticketId, function(results) {
+                   var history = "";
+                   for (var i = 0; i < results.length; ++i) {
+                       var result = results[i];
+                       history += result["msg_sender"].replace("alfred", ticketData.name) + ": " + result["msg_text"] + "*np*";
+                   }
+
+                   history = history.replace("\n", "*br*");
+                   res.render('userResponse', { historyBlock: history, name: ticketData.name, title: ticketData.title });
+               }, function (err) {
+                   res.render('error');
+               });
+           }, function(err) {
+               res.render('error');
+           });
+
+       }
+    });
 
 });
 
