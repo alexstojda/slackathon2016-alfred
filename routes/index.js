330c2649ee5d332848392b6220d39074287de854
GabrielAlacchi <alacchi.g@gmail.com>
2016-06-05 01:47:07 -0400

    This commit message has no bearing on my life

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var ticketDB = require('../source/TicketDatabase');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'El Slackos' });
+    var customFields = ticketDB.getCustomFields();
+    var jsonCFields = JSON.stringify(customFields);
+    res.render('index', { title: 'El Slackos', CustomFieldsJson: jsonCFields });
 });
 
 module.exports = router;
